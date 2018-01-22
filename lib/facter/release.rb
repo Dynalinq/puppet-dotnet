866b421508562117dec837ea56de9a61613df319
Robby Holsteyns <robby.holsteyns@gmail.com>
2018-01-22 16:48:35 +0100

    Fix issues with release.rb script

--- lib/facter/release.rb
+++ lib/facter/release.rb
@@ -1,13 +1,10 @@
-Facter.add(:dotnet_release) do
-    confine :kernel => :windows
-    setcode do
-      require 'win32/registry'
-  
-      value = nil
-      hive = Win32::Registry::HKEY_LOCAL_MACHINE
-      hive.open(*'*SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full',  Win32::Registry::KEY_READ | 0x100) do |reg|
-        value = reg['release']
-      end
-      value
+Facter.add('dotnet_release') do
+  confine :osfamily => :windows
+  setcode do
+    value = nil
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full') do |regkey|
+      value = regkey['Release']
     end
-  end    
+    value
+  end
+ end  
